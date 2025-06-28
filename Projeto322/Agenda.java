d30c97b8f087d0bd1587b884ee8d80863a0de8fa
MLeticiagomes <m281318@dac.unicamp.br>
2025-06-28 16:01:49 -0300

    agenda e atendimentos

--- Projeto322/Agenda.java
+++ Projeto322/Agenda.java
@@ -1,3 +1,36 @@
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class Agenda {
-    
+   private Map <LocalDate, Atendimento > atendimentos;
+  
+   public Agenda(){
+       atendimentos= new HashMap<>();
+   }
+
+
+   public void agendar (LocalDate data, Procedimento procedimento){
+       Atendimento a = atendimentos.getOrDefault(data, new Atendimento(data));
+       a.adicionarProcedimento(procedimento);
+       atendimentos.put(data,a);
+   }
+  
+   public Atendimento getAtendimentoDia (LocalDate data){
+       return  atendimentos.get(data);
+   }  
+
+
+   public Map<LocalDate, Atendimento> getTodosAtendimentos(){
+       return atendimentos;
+   }
+
+
 }
+
+
+
+
+
+
--- Projeto322/Atendimento.java
+++ Projeto322/Atendimento.java
@@ -1,3 +1,52 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Atendimento {
-    
+   private LocalDate data;
+   private List <Procedimento> procedimentos;
+   private String paciente;
+
+
+   public Atendimento (LocalDate data){
+       this.data = data;
+       this.procedimentos  = new ArrayList<>();
+       this.paciente = paciente;
+   }
+
+
+   public LocalDate getData(){
+       return data;
+   }
+
+
+   public List<Procedimento> getProcedimentos() {
+       return procedimentos;
+   }
+
+
+   public String getPaciente(){
+       return paciente;
+  }
+   public void adicionarProcedimento(Procedimento p) {
+       procedimentos.add(p);
+   }
+
+
+
+
+   public void removerProcedimento(Procedimento p) {
+       procedimentos.remove(p);
+   }
+
+
+   public double valorFinal ( Procedimento procedimentos){
+       double valorFixo = 0;
+       double valorEquipamentos = procedimentos.calcularValor();
+       double valorFInal = valorFixo + valorEquipamentos;
+       return valorFInal;
+   }
+
+
 }
